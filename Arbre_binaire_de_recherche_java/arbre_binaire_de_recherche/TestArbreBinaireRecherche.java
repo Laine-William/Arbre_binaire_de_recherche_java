9890619c0f9e3273f079d2bbd81e319b03c2995c
lwdu76 <laine.william01@gmail.com>
2023-01-31 19:06:06 +0100

    update

--- Arbre_binaire_de_recherche_java/arbre_binaire_de_recherche/TestArbreBinaireRecherche.java
+++ Arbre_binaire_de_recherche_java/arbre_binaire_de_recherche/TestArbreBinaireRecherche.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class TestArbreBinaireRecherche {
+public class Test {
     public static void main(String[] args) {
 
         ArbreBinaireRecherche arbre=new ArbreBinaireRecherche();
