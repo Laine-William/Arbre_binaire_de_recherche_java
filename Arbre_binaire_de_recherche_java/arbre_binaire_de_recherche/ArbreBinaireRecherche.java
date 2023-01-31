d1b1ca244f32e42324b5ecb914ab8fc76e2c2578
lwdu76 <laine.william01@gmail.com>
2023-01-31 19:05:36 +0100

    update

--- Arbre_binaire_de_recherche_java/arbre_binaire_de_recherche/ArbreBinaireRecherche.java
+++ Arbre_binaire_de_recherche_java/arbre_binaire_de_recherche/ArbreBinaireRecherche.java
@@ -1,7 +1,4 @@
-/** Une classe pour créer un arbre binaire de recherche
- * @author Munkhdorj Erdenebaatar, William Laine
- * @version 03/05/2021
- */
+// Une classe pour créer un arbre binaire de recherche
 
 public class ArbreBinaireRecherche {
     /**
@@ -17,6 +14,8 @@ public class ArbreBinaireRecherche {
      * Constructeur de l'arbre binaire
      */
     public ArbreBinaireRecherche() {
+
+        this.courant = null;
         this.racine=null;
     }
 
@@ -299,54 +298,5 @@ public class ArbreBinaireRecherche {
      */
     public Cellule getRacine(){
         return this.racine;
-    }
-
-    /**Classe privée pour créer les noeuds d'un arbre bibaire de recherche
-     */
-    private class Cellule {
-        /** le contenu d'un noeud
-         */
-        Element contenu ; // contenu d'une cellule
-        /** le noeud gauche du noeud courant
-         */
-        Cellule filsG ; // La cellule qui est le fils gauche
-        /** le noeud droit du noeud courant
-         */
-        Cellule filsD ; // la cellule qui est le fils droit
-        /**Supprimer le noeud parent du noeud courant
-         */
-        Cellule parent ; // la cellule qui est le parent
-
-        /** Constructeur d'un noeud
-         * @param contenu le contenu du noeud à créer
-         */
-        Cellule(Element contenu) {
-            this.contenu=new Element(contenu);
-            this.filsG =null;
-            this.filsD=null;
-            this.parent=null;
-        }
-
-        /** Constructeur d'un noeud avec 2 paramètres
-         * @param contenu le contenu du noeud à créer
-         * @param parent le noeud qui va être le parent
-         */
-        Cellule(Element contenu, Cellule parent) {
-            this.contenu=new Element(contenu);
-            this.filsG =null;
-            this.filsD=null;
-            this.parent=parent;
-        }
-
-        void setContenu(Element e){
-            this.contenu = new Element(e);
-        }
-
-        /** Affichage  du contenu d'un noeud
-         * @return String
-         */
-        public String toString(){
-            return this.contenu.toString();
-        }
     }
 }
---
+++ Arbre_binaire_de_recherche_java/arbre_binaire_de_recherche/Cellule.java
@@ -0,0 +1,47 @@
+// Classe  pour créer les noeuds d'un arbre bibaire de recherche
+    private class Cellule extends ArbreBinaireRecherche {
+        /** le contenu d'un noeud
+         */
+        Element contenu ; // contenu d'une cellule
+        /** le noeud gauche du noeud courant
+         */
+        Cellule filsG ; // La cellule qui est le fils gauche
+        /** le noeud droit du noeud courant
+         */
+        Cellule filsD ; // la cellule qui est le fils droit
+        /**Supprimer le noeud parent du noeud courant
+         */
+        Cellule parent ; // la cellule qui est le parent
+
+        /** Constructeur d'un noeud
+         * @param contenu le contenu du noeud à créer
+         */
+        Cellule(Element contenu) {
+            this.contenu=new Element(contenu);
+            this.filsG =null;
+            this.filsD=null;
+            this.parent=null;
+        }
+
+        /** Constructeur d'un noeud avec 2 paramètres
+         * @param contenu le contenu du noeud à créer
+         * @param parent le noeud qui va être le parent
+         */
+        Cellule(Element contenu, Cellule parent) {
+            this.contenu=new Element(contenu);
+            this.filsG =null;
+            this.filsD=null;
+            this.parent=parent;
+        }
+
+        void setContenu(Element e){
+            this.contenu = new Element(e);
+        }
+
+        /** Affichage  du contenu d'un noeud
+         * @return String
+         */
+        public String toString(){
+            return this.contenu.toString();
+        }
+    }
